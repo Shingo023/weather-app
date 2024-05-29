31353a8f596543eb4483331c23a436586fff4411
Shingo023 <s22sugi@gmail.com>
2024-05-29 15:07:47 -0700

    Finish making the autocomplete search bar for cities.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,34 @@
 "use client";
 
-import { useRef, useEffect } from "react";
-// The 'useLoadScript' hook handles the loading of the Google Maps JavaScript API script in a React application.
-import { useLoadScript } from "@react-google-maps/api";
+import { useRef, useState, useEffect } from "react";
+import { useJsApiLoader, LoadScriptProps } from "@react-google-maps/api";
 
-// The "places" library is necessary for autocomplete for addresses and places.
-const libraries: "places"[] = ["places"];
+// "places" library: necessary for autocomplete for addresses and places
+const libraries: LoadScriptProps["libraries"] = ["places"];
 
 const SearchBar = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [librariesArray] = useState<LoadScriptProps["libraries"]>(libraries);
 
-  const { isLoaded, loadError } = useLoadScript({
+  // Load Google Maps API
+  const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-    libraries,
+    libraries: librariesArray,
   });
 
   useEffect(() => {
     if (isLoaded && inputRef.current) {
-      // Set up an autocomplete feature for an input element
+      // Add an autocomplete feature to the input field
       const autocomplete = new google.maps.places.Autocomplete(
         inputRef.current,
         {
           types: ["(cities)"],
-          fields: ["address_components", "geometry", "name"],
+          // fields: ["address_components", "geometry", "name"],
+          fields: ["name"],
         }
       );
 
-      // 'addListener' is used to add event listeners to Google Maps objects like autocomplete instances.
+      // 'addListener' is used to add event listeners to Google Maps objects (e.g., 'autocomple').
       autocomplete.addListener("place_changed", () => {
         const place = autocomplete.getPlace();
         console.log("Selected place:", place);
