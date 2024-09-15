61273042a9ff2dbbe151f7e04a066d29724c0772
Shingo023 <s22sugi@gmail.com>
2024-09-15 10:46:33 -0700

    Create a register page.

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  try {
+    const { name, email, password } = await request.json();
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          message:
+            "The email address is already registered. Try a different one.",
+        },
+        { status: 400 }
+      );
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Registration successful!" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal server error." },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import React, { useState, FormEvent } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { useRouter } from "next/navigation";
+
+export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError("");
+
+    // Define password strength requirements
+    const passwordRegex = /^(?=.*[\d!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
+
+    // Check if password meets requirements
+    if (!passwordRegex.test(password)) {
+      setError(
+        "Password must be at least 6 characters long, and include at least one number or special character, e.g., !@#$%^&*."
+      );
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      if (!response.ok) {
+        const text = await response.text();
+        const data = text ? JSON.parse(text) : {};
+        throw new Error(data.message || "An error occurred");
+      }
+
+      // Parse success message from server response
+      const data = await response.json();
+      toast.success(data.message || "Registration successful!");
+
+      // Redirect to login page after a short delay
+      setTimeout(() => {
+        router.push("/login");
+      }, 3000);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        toast.error(error.message);
+      } else {
+        setError("An unexpected error occurred.");
+        toast.error("An unexpected error occurred.");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <ToastContainer />
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Email
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Password
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <p>
+            at least 6 characters; must include a number or special character,
+            e.g., !@#$%^&*
+          </p>
+        </div>
+
+        <div>
+          <label>
+            Confirm Password
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+
+        <div>
+          <button type="submit">Register</button>
+        </div>
+
+        <div>{error && <p>{error}</p>}</div>
+      </form>
+    </div>
+  );
+}
